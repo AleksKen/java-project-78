43cf55487e35bf7af6ada199960160e1072a42c0
AleksKen <aleks.ken2017@mail.ru>
2024-08-30 13:51:38 +0300

    Вложенная валидация (без теста num)

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -4,17 +4,30 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public class MapSchema extends BaseSchema {
+public class MapSchema<T> extends BaseSchema<Map> {
 
-    public MapSchema required() {
+    public MapSchema<T> required() {
         Predicate<Map> lambda = Objects::nonNull;
         checks.put("required", lambda);
         return this;
     }
 
-    public MapSchema sizeof(int size) {
+    public MapSchema<T> sizeof(int size) {
         Predicate<Map> lambda = x -> x.size() == size;
         checks.put("sizeof", lambda);
+        return this;
+    }
+
+    public <V> MapSchema<T> shape(Map<String, BaseSchema<V>> schemas) {
+        Predicate<Map> lambda = x -> {
+            for (var key : schemas.keySet()) {
+                if (!schemas.get(key).isValid((V) x.get(key))) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        checks.put("shape", lambda);
         return this;
     }
 }
--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -1,15 +1,17 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MapSchemaTest {
     @Test
-    public void test() {
+    public void testCore() {
         var v = new Validator();
         var schema = v.map();
 
@@ -32,5 +34,30 @@ public class MapSchemaTest {
         var schema1 = v.map();
 
         assertTrue(schema1.sizeof(5).sizeof(2).isValid(data));
+    }
+
+    @Test
+    public void testNested() {
+        var v = new Validator();
+        var schema = v.map();
+        Map<String, BaseSchema<String>> schemas = new HashMap<>();
+        schemas.put("firstName", v.string().required());
+        schemas.put("lastName", v.string().required().minLength(2));
+        schema.shape(schemas);
+
+        Map<String, String> human1 = new HashMap<>();
+        human1.put("firstName", "John");
+        human1.put("lastName", "Smith");
+        assertTrue(schema.isValid(human1));
+
+        Map<String, String> human2 = new HashMap<>();
+        human2.put("firstName", "John");
+        human2.put("lastName", null);
+        assertFalse(schema.isValid(human2));
+
+        Map<String, String> human3 = new HashMap<>();
+        human3.put("firstName", "Anna");
+        human3.put("lastName", "B");
+        assertFalse(schema.isValid(human3));
     }
 }
